5279a7ec081f75625e1f2905ffe0bbb587e1cd45
Magichear <wgdecade43@mail.ustc.edu.cn>
2025-04-09 14:40:17 +0800

    [Start] Have an great idea of how to crawl the data of https://www.imf.org

---
+++ t1.py
@@ -0,0 +1,23 @@
+baseUrl = "https://www.imf.org/en/Publications/WEO/weo-database/2024/October/weo-report?"
+
+grp = [995, 119]
+
+
+https://www.imf.org/en/Publications/WEO/weo-database/2024/October/select-country-group
+
+
+https://www.imf.org/en/Publications/WEO/weo-database/2024/October/select-countries?grp=995&sg=All-countries/Advanced-economies/Euro-area
+
+---
+
+https://www.imf.org/en/Publications/WEO/weo-database/2024/October/select-subjects?c=122,124,960,423,939,172,132,134,174,178,136,941,946,137,181,138,182,936,961,184,
+
+https://www.imf.org/en/Publications/WEO/weo-database/2024/October/select-date-range?c=122,124,960,423,939,172,132,134,174,178,136,941,946,137,181,138,182,936,961,184,&s=NGDPD,
+
+---
+
+https://www.imf.org/en/Publications/WEO/weo-database/2024/October/weo-report?c=122,124,960,423,939,172,132,134,174,178,136,941,946,137,181,138,182,936,961,184,&s=NGDPD,&sy=1990&ey=2022&ssm=0&scsm=1&scc=1&ssd=1&ssc=0&sic=0&sort=country&ds=.&br=1
+
+https://www.imf.org/en/Publications/WEO/weo-database/2024/October/weo-report?c=156,132,134,136,158,112,111,&s=NGDPD,&sy=1990&ey=2022&ssm=0&scsm=1&scc=1&ssd=1&ssc=0&sic=0&sort=country&ds=.&br=1
+
+https://www.imf.org/en/Publications/WEO/weo-database/2024/October/weo-report?c=513,514,516,522,924,819,534,536,826,544,548,556,867,868,948,518,836,558,565,853,566,862,813,524,578,537,866,869,846,582,&s=NGDPD,&sy=1990&ey=2022&ssm=0&scsm=1&scc=1&ssd=1&ssc=0&sic=0&sort=country&ds=.&br=1
